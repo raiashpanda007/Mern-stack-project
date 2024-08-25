a733d463ff0fb07c4c66891e8227ab9e079a4b8e
Ashwin Rai <ashwin.2201098cs@iiitbh.ac.in>
2024-08-25 12:08:56 +0530

    Made sliding of sidebar more smooth for home

--- Frontend/src/Screens/Home.jsx
+++ Frontend/src/Screens/Home.jsx
@@ -9,18 +9,16 @@ function Home() {
     <>
       <Header />
       <div className="w-full flex">
-        {/* Conditionally render Sidebar */}
-        {Bar && (
-          <div className="w-1/5 relative top-16 h-screen">
-            <Sidebar />
-          </div>
-        )}
+        {/* Sidebar with smooth sliding */}
+        <div className={`relative top-16 ${Bar ? "w-1/5" : "w-0"} transition-all duration-300`}>
+          {Bar && <Sidebar />}
+        </div>
 
         {/* Main Content */}
         <div
-          className={`${
-            Bar ? 'w-4/5' : 'w-full'
-          } flex flex-wrap overflow-y-auto h-screen relative top-16 p-4 gap-4`}
+          className={`relative top-16 flex flex-wrap overflow-y-auto h-screen p-4 gap-4 ${
+            Bar ? "w-4/5" : "w-full"
+          } transition-all duration-300`}
         >
           {/* Rendering multiple HomeCard components */}
           {Array(12).fill().map((_, index) => (
