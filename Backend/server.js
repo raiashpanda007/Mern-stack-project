6d1127426e1273764aec69cd09ab5d4cef892267
Ashwin Rai <ashwin.2201098cs@iiitbh.ac.in>
2024-09-10 22:08:45 +0530

    Start backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,9 @@
 import express from "express";
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+import app from "./app.js";
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+    console.log("⚙️ Server is running at port");
+
 });
+
